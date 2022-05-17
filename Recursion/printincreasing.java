9148374c65257e677b414ba6d784b07e0ca4c037
Avinash-ray <rayavinash616@gmail.com>
2022-05-17 15:47:49 +0530

    printincreasing

--- Recursion/printincreasing.java
+++ Recursion/printincreasing.java
@@ -1,3 +1,31 @@
+// 1. You are given a positive number n. 
+// 2. You are required to print the counting from 1 to n.
+// 3. You are required to not use any loops. 
+
+// Constraints
+// 1 <= n <= 1000
+
+// Input
+// A number n
+
+// Output
+// 1
+// 2
+// 3
+// ..
+// n
+
+// Example
+// Sample Input
+// 5
+
+// Sample Output
+// 1
+// 2
+// 3
+// 4
+// 5
+
 import java.util.Scanner;
 
 public class Main {
