1437ee90ae0ac326a9e8269de45c245468fe23d6
Avinash-ray <rayavinash616@gmail.com>
2022-05-19 21:23:40 +0530

    print z

---
+++ Basic/print-z.java
@@ -0,0 +1,25 @@
+// 1. You are required to print a 'z' of size 5 using '*'.
+
+// Format
+// Input
+// There is no input
+
+// Output
+// *****
+//    *
+//   *
+//  *
+// *****
+
+public class Main {
+
+  public static void main(String[] args) {
+    // Write your code here
+    System.out.println("*****");
+    System.out.println("   *");
+    System.out.println("  *");
+    System.out.println(" *");
+    System.out.println("*****");
+    
+  }
+}
