8db34625496f40b99cadb3f83592fe2caf9523d5
Avinash-ray <rayavinash616@gmail.com>
2022-05-26 16:10:19 +0530

    for and while loop

---
+++ Basic/loops.java
@@ -0,0 +1,15 @@
+public class Main {
+
+  public static void main(String[] args) {
+    // Write your code here
+    // for(int i=1; i<=10; i++){
+    //   System.out.println(i);
+    // }
+
+    int i=0; 
+    while(i<=10){
+      System.out.println(i);
+    }
+
+  }
+}
