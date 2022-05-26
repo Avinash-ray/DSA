3226a9b515f7caab06668cfd0220c8f2d4270fd6
Avinash-ray <rayavinash616@gmail.com>
2022-05-26 16:19:06 +0530

    write for loop like while

--- Basic/loops.java
+++ Basic/loops.java
@@ -1,14 +1,20 @@
 public class Main {
 
   public static void main(String[] args) {
-    // Write your code here
-    // for(int i=1; i<=10; i++){
-    //   System.out.println(i);
-    // }
+    Write your code here
+    for(int i=1; i<=10; i++){
+      System.out.println(i);
+    }
 
     int i=0; 
     while(i<=10){
       System.out.println(i);
+    }
+
+     int i=1;
+    for(; i<=10;){
+      System.out.println(i);
+      i++;
     }
 
   }
