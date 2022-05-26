e51c552619ae5154048ed75d176ca2e8ace28f80
Avinash-ray <rayavinash616@gmail.com>
2022-05-26 17:05:54 +0530

    fibonacci series

---
+++ Basic/fibonacci.java
@@ -0,0 +1,45 @@
+// 1. You've to print first n fibonacci numbers.
+// 2. Take as input "n", the count of fibonacci numbers to print.
+// 3. Print first n fibonacci numbers.
+
+// Constraints
+// 1 <= n < 40
+
+// Example
+// Sample Input
+
+// 10
+
+// Sample Output
+// 0
+// 1
+// 1
+// 2
+// 3
+// 5
+// 8
+// 13
+// 21
+// 34
+
+import java.util.Scanner;
+public class Main {
+
+  public static void main(String[] args) {
+  Scanner scn= new Scanner(System.in);
+  int n= scn.nextInt();
+
+    int count= 1;
+    int a= 0;
+    int b= 1;
+    
+    while(count <= n){
+      int sum= a+b;
+      System.out.println(sum);
+      a= b;
+      b= sum;
+      count++;
+    }
+
+  }
+}
